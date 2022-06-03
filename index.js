29fdc34a106b4a890fba31642066b89e9308b870
SAIF-ANWAR <saifanwar5123@gmail.com>
2022-06-04 02:01:23 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.izrfy.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+
+    try {
+        await client.connect()
+        const mobileCollection = client.db("mobiles").collection("mobile");
+
+        app.get('/mobiles', async (req, res) => {
+            const query = {}
+            const cursor = mobileCollection.find(query)
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+    } finally {
+
+    }
+
+}
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
+
